46a06c4d0e057b1997875b26669b47639321d278
sparadeloweb <santiagoparadelo123@gmail.com>
2023-09-26 20:09:53 -0300

    Fix .gitignore /storage/framework y /storage/logs

--- application/storage/framework/views/17882ef08c639b0ee7c8daa5daa9037dd4a40ec5.php
+++
@@ -1,2 +0,0 @@
-<!--main table view-->
-<?php echo $__env->make('pages.categories.components.table.table', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\laragon\www\Chaman\application\resources\views/pages/categories/components/table/wrapper.blade.php ENDPATH**/ ?>
--- application/storage/framework/views/81bf6fa0d15c2df10dcc03a4abab20e826df0ac3.php
+++
@@ -1,7 +0,0 @@
-<div class="form-group row">
-    <label for="example-month-input" class="col-12 col-form-label text-left"><?php echo e(cleanLang(__('lang.project'))); ?></label>
-    <div class="col-12">
-        <select name="attach_project_id" id="attach_project_id" class="form-control form-control-sm js-select2-basic-search select2-hidden-accessible"
-        data-ajax--url="<?php echo e($payload['projects_feed_url']); ?>"></select>
-    </div>
-</div><?php /**PATH C:\laragon\www\Chaman\application\resources\views/pages/invoices/components/actions/attach-project.blade.php ENDPATH**/ ?>
