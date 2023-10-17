c84e4743b8ffba03fdb780cbe4c02993aeac9675
Martin Santamaria <msantamaria@mvdcreativestudio.com>
2023-10-17 08:07:40 -0300

    Martes 17/10

--- application/resources/views/pages/home/admin/widgets/first-row/goals.blade.php
+++ application/resources/views/pages/home/admin/widgets/first-row/goals.blade.php
@@ -54,86 +54,13 @@
     <!-- Flecha -->
     <i class="ti-angle-double-right arrow"></i>
 </div>
-<div class="text-right">
-    <form method="post" action="{{ url('/update-progress-for-all-objectives') }}">
-        @csrf
-        <button class="btn btn-primary mt-0" type="submit">Recargar Progreso</button>
-    </form>
-</div>
+
 
 
 <div id="alert-container" class="alert-container"></div>
 
 
 <div id="chart"></div>
-
-
-
-
-
-
-<script src="https://d3js.org/d3.v5.min.js"></script>
-<script src="https://unpkg.com/c3@0.7.20/c3.min.js"></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function () {
-        // Datos de ejemplo (puedes reemplazarlos con tus propios datos)
-        
-        var chart = c3.generate({
-    data: {
-        columns: [
-            ['data1', 300, 350, 300, 0, 0, 0],
-            ['data2', 130, 100, 140, 200, 150, 50]
-        ],
-        types: {
-            data1: 'area',
-            data2: 'area-spline'
-        }
-    }
-});
-
-
-    });
-</script>
-
-
-
-{{-- <div class="col-12">
-    <div class="row">
-        <div class="col-12 d-flex scrollable-container">
-            @foreach($payload['objectives'] as $objective)
-            <!-- Tarjeta Dinámica -->
-            <div class="col-lg-4 col-md-4 col-6 cursor-pointer">
-                <div class="card">
-                    <div class="col-lg-4 col-md-4 col-6 cursor-pointer pr-0">
-                        <div class="d-flex p-10 no-block justify-content-between">
-                            <span class="align-slef-center">
-                                <h4 class="m-b-0">{{ $objective->name }}</h4>
-                                <p class="objective text-muted p-0 mb-1">{{ $objective->description }}</p>
-                                <h6 class="text-muted m-b-0">{{ $objective->progress }}%</h6>
-                            </span>
-                            <span class="align-slef-center">
-                                <p>Estado: {{ $objective->status }}</p>
-                            </span>
-                        </div>
-                    </div>
-                    <div class="progress2">
-                        <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: {{ $objective->progress }}%" aria-valuenow="{{ $objective->progress }}" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-            </div>
-            @endforeach
-        </div>
-    </div>
-    <!-- Flecha -->
-    <i class="ti-angle-double-right arrow"></i>
-</div> --}}
-
-
-
-<!-- Invoice - Due-->
-
-
-
 
 
 
