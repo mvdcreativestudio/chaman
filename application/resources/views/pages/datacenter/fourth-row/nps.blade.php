e4ddb3fa568739f8632d6c42bf253fcf80553b49
Manuel I Fernandez <manuel_ignacio95@hotmail.com>
2023-10-31 03:27:54 -0300

    nps

--- application/resources/views/pages/datacenter/fourth-row/nps.blade.php
+++ application/resources/views/pages/datacenter/fourth-row/nps.blade.php
@@ -1,12 +1,3 @@
 <div class="col-lg-3 col-md-12">
-<div class="rating-star">
-    <h5 class="card-title m-b-0 align-self-center pb-4">NPS</h5>
-    <div class="stars">
-     <i class="ti-star"></i>
-     <i class="ti-star"></i>
-     <i class="ti-star"></i>
-     <i class="ti-star"></i>
-     <i class="ti-star"></i>
-    </div>
-</div>
+
 </div>
